e787bcd5e38c9b68980626d7822d441dd9f946bf
Kiran Merchant <kiranmerchant@kirans-mbp.devices.brown.edu>
2018-12-11 17:56:27 -0500

    Updates

---
+++ precache-manifest.f301dc98ef38e05be94d4b304025aae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "084ac45aee39707f976e",
+    "url": "/cv/static/js/runtime~main.084ac45a.js"
+  },
+  {
+    "revision": "089d67ea24a20f593050",
+    "url": "/cv/static/js/main.089d67ea.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/cv/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "089d67ea24a20f593050",
+    "url": "/cv/static/css/main.7b78eb88.chunk.css"
+  },
+  {
+    "revision": "8944279dbe9d0c29ff18757ae0834618",
+    "url": "/cv/index.html"
+  }
+];
